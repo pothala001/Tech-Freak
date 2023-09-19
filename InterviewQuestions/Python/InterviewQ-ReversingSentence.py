b129560d36527bd0b4c1bdd5513ab0a5ef25573f
pothala001 <35107258+pothala001@users.noreply.github.com>
2023-09-20 00:00:34 +0530

    Update InterviewQ-ReversingSentence.py

--- InterviewQuestions/Python/InterviewQ-ReversingSentence.py
+++ InterviewQuestions/Python/InterviewQ-ReversingSentence.py
@@ -1,3 +1,4 @@
+# Write a program to reverse the a string without using reverse built-in function
 given_sentence = 'I am learning python'
 word_list = given_sentence.split(' ')
 
@@ -6,5 +7,5 @@ reversed_string = ''
 for x in word_list[::-1]:
     reversed_string += x + ' '
 
-print(reversed_string)
-print(given_sentence[::-1])
+print(reversed_string) #expected output: 'python learning am I' 
+print(given_sentence[::-1]) #expected output: 'nohtyp gninrael ma I'
